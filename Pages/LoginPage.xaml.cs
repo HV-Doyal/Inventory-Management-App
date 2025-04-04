ef97620fe895d52e99937581f007baf3d5d0e875
harsh <hd499@live.mdx.ac.uk>
2025-04-04 23:15:56 +0400

    Added logic to login page

--- Pages/LoginPage.xaml.cs
+++ Pages/LoginPage.xaml.cs
@@ -1,4 +1,7 @@
 using UndergradProject.Pages;
+using UndergradProject.Business_Logic_Layer;
+using UndergradProject.Data_Access_Layer;
+using System.Threading.Tasks;
 namespace UndergradProject;
 
 public partial class LoginPage : ContentPage
@@ -8,10 +11,35 @@ public partial class LoginPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void loginButton_Clicked(object sender, EventArgs e)
+    private async void loginButton_Clicked(object sender, EventArgs e)
     {
+        string username = usernameEntry.Text?.Trim();
+        string password = passwordEntry.Text?.Trim();
+        UserMangement userMangement = new UserMangement();
+        await userMangement.initialiseUsersTable();
 
+        // Check if username or password is null or whitespace
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        {
+            await DisplayAlert("Error", "Username or password cannot be empty!", "OK");
+            return; 
+        }
+
+        List<User> users = await userMangement.getAllUsersFromDatabase();
+
+        if (await userMangement.isAccountValid(username, password))
+        {
+            await DisplayAlert("Success", "Login successful!", "OK");
+            await Navigation.PushModalAsync(new Dashboard());
+        }
+        else
+        {
+            await DisplayAlert("Error", "Invalid username or password!", "OK");
+            usernameEntry.Text = string.Empty;
+            passwordEntry.Text = string.Empty;
+        }
     }
+
 
     private async void signupInsteadButton_Clicked(object sender, EventArgs e)
     {
