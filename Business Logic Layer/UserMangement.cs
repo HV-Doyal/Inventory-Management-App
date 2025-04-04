36ce8899ce2d4b868f1836fc0d29d01e09fae893
harsh <hd499@live.mdx.ac.uk>
2025-04-04 15:36:28 +0400

    Added a userManagement class to manage object user

---
+++ Business Logic Layer/UserMangement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UndergradProject.Data_Access_Layer;
+
+namespace UndergradProject.Business_Logic_Layer
+{
+    public class UserMangement
+    {
+        public User createUser(string username, string email, string password)
+        {
+            User user = new User(username,email, password);
+            Console.WriteLine("User created successfully!");
+            Console.WriteLine($"UserID: {user.userId}, Username: {user.Username}, Email: {user.Email}, Password: {user.Password}");
+            return user;
+        }
+    }
+}
