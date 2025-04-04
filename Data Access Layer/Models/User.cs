f2d213436483a037cca04134b39c75404430b271
harsh <hd499@live.mdx.ac.uk>
2025-04-04 14:10:14 +0400

    Added a User Class

---
+++ Data Access Layer/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndergradProject.Data_Access_Layer
+{
+    class User
+    {
+        // Properties
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        // Constructor
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
