351b4cc1afa47d6653b634e48dc8e6ec9080d43a
harsh <hd499@live.mdx.ac.uk>
2025-04-04 15:36:48 +0400

    Added a constant class to store all contants

---
+++ Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndergradProject
+{
+   public static class Constants
+   {
+        public static readonly Color darkGrayColor = Color.FromArgb("#1E1D1D");
+        public static readonly Color backgroundColor = Color.FromArgb("#121212");
+        public static readonly Color whiteColor = Color.FromArgb("#FFFFFF");
+    }
+}
