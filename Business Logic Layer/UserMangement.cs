27ea59bafb61e1e7e0c8bb176305e3f61a752c78
harsh <hd499@live.mdx.ac.uk>
2025-04-04 21:53:50 +0400

    Added when creating new user, it check if user exists then adds else giving error

--- Business Logic Layer/UserMangement.cs
+++ Business Logic Layer/UserMangement.cs
@@ -9,12 +9,62 @@ namespace UndergradProject.Business_Logic_Layer
 {
     public class UserMangement
     {
-        public User createUser(string username, string email, string password)
+        DatabaseService databaseUserService = new DatabaseService(Constants.databasePath);
+
+        //Initialise the table in the databse
+        public async Task initialiseUsersTable()
         {
-            User user = new User(username,email, password);
+            // Create the table for User
+            await databaseUserService.CreateTableAsync<User>();
+        }
+
+        //Add user to table in database
+        public async Task addUserToDatabase(User user)
+        {
+            await initialiseUsersTable();
+            await databaseUserService.InsertDataAsync(user);
+        }
+
+        public async Task<bool> createUser(string username, string email, string password)
+        {
+            User user = new User(username, email, password);
             Console.WriteLine("User created successfully!");
             Console.WriteLine($"UserID: {user.userId}, Username: {user.Username}, Email: {user.Email}, Password: {user.Password}");
-            return user;
+           
+            if (!await IsUserPresentInDatabase(user))
+            {
+                // Add user to the database
+                await addUserToDatabase(user);
+                Console.WriteLine("User added to database successfully!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("User already present in database!");
+                return false;
+            }
+        }
+
+        //returns all users from database
+        public async Task<List<User>> getAllUsersFromDatabase()
+        {
+            List<User> users = await databaseUserService.GetDataAsync<User>();
+
+            foreach (var u in users)
+            {
+                Console.WriteLine($"UserID: {u.userId}, Username: {u.Username}, Email: {u.Email}, Password: {u.Password}");
+            }
+
+            return users;
+        }
+
+        //Checks is user is present in Databse
+        public async Task<bool> IsUserPresentInDatabase(User user)
+        {
+            var users = await getAllUsersFromDatabase();
+            return users.Any(u =>
+                string.Equals(u.Email, user.Email, StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(u.Username, user.Username, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
