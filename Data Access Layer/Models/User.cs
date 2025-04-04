e42dcac023fd6857bee62c7c69e98fa392879874
harsh <hd499@live.mdx.ac.uk>
2025-04-04 15:38:24 +0400

    Added SQLite for managing database

--- Data Access Layer/Models/User.cs
+++ Data Access Layer/Models/User.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace UndergradProject.Data_Access_Layer
 {
-    class User
+    public class User
     {
         // Properties
+        [PrimaryKey, AutoIncrement]
+        public int userId { get; set; }
+
+        [MaxLength(100)]
         public string Username { get; set; }
+
+        [MaxLength(100)]
         public string Email { get; set; }
+
+        [MaxLength(100)]
         public string Password { get; set; }
 
         // Constructor
