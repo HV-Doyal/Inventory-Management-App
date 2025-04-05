7ecf2f96f14b0be3b82e1ccd068d4b6bcad2763b
harsh <hd499@live.mdx.ac.uk>
2025-04-06 01:25:06 +0400

    Added function to scan button

--- Pages/AddItemPage.xaml.cs
+++ Pages/AddItemPage.xaml.cs
@@ -7,9 +7,17 @@ public partial class AddItemPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void scanButton_Clicked(object sender, EventArgs e)
+    private async void scanButton_Clicked(object sender, EventArgs e)
     {
-
+        var status = await Permissions.RequestAsync<Permissions.Camera>();
+        if (status == PermissionStatus.Granted)
+        {
+            await Navigation.PushModalAsync(new BarcodeScannerPage());
+        }
+        else
+        {
+            await DisplayAlert("Permission Denied", "Camera permission is required to scan barcodes. Please enable it in the app settings.", "OK");
+        }
     }
 
     private void addItemButton_Clicked(object sender, EventArgs e)
