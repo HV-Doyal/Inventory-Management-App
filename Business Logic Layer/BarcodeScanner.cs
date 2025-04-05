17af09c44f30f2b9ec6a92d853b219800775cd82
harsh <hd499@live.mdx.ac.uk>
2025-04-06 01:26:18 +0400

    Created a class barcodeScanner to handle barcode management

---
+++ Business Logic Layer/BarcodeScanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndergradProject.Business_Logic_Layer
+{
+    class BarcodeScanner
+    {
+    }
+}
