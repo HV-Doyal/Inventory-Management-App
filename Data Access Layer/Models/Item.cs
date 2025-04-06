35fc3027414a81879342c02a9e5881e6f558ba0a
harsh <hd499@live.mdx.ac.uk>
2025-04-06 16:25:37 +0400

    Added an object of type item

---
+++ Data Access Layer/Models/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace UndergradProject.Data_Access_Layer.Models
+{
+    public class Item
+    {
+        // Properties
+        [PrimaryKey, AutoIncrement]
+        public int itemId { get; set; }
+
+        [MaxLength(100)]
+        public string name { get; set; }
+
+        [MaxLength(100)]
+        public string category { get; set; }
+
+        [MaxLength(100)]
+        public string barcode { get; set; }
+
+        public int quantity { get; set; }
+        public double unitPrice { get; set; }
+
+        // Parameterless constructor
+        public Item() { }
+
+        // Constructor without itemId
+        public Item(string name, string category, string barcode, int quantity, double unitPrice)
+        {
+            this.name = name;
+            this.category = category;
+            this.barcode = barcode;
+            this.quantity = quantity;
+            this.unitPrice = unitPrice;
+        }
+    }
+}
