d618342040852e0b12ece2f57d00728870ea4d4e
harsh <hd499@live.mdx.ac.uk>
2025-04-06 16:28:47 +0400

    Added path for items table

--- Constants.cs
+++ Constants.cs
@@ -11,6 +11,7 @@ namespace UndergradProject
         public static readonly Color darkGrayColor = Color.FromArgb("#1E1D1D");
         public static readonly Color backgroundColor = Color.FromArgb("#121212");
         public static readonly Color whiteColor = Color.FromArgb("#FFFFFF");
-        public static readonly string databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "users.db3");
+        public static readonly string usersDatabasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "users.db3");
+        public static readonly string itemsDatabasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "items.db3");
     }
 }
